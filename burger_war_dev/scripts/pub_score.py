2820b80bed23ca3c8d325d0f556f3e841777ac25
BolaDeArroz <kawai.takayasu@jp.panasonic.com>
2021-03-13 15:37:30 +0900

    fix add side info

--- burger_war_dev/scripts/pub_score.py
+++ burger_war_dev/scripts/pub_score.py
@@ -14,17 +14,23 @@ from std_msgs.msg import String
 
 class PubScore():
     def __init__(self):
+        self.side = str(rospy.get_param("~side"))
+
         # markers name: order is decided by zyali's board
         self.markers_name_list = [
-            'Tomato_N', 'Omelette_N',
-            'Tomato_S', 'Omelette_S', 
-            'FriedShrimp_N','FriedShrimp_W', 
-            'FriedShrimp_E','FriedShrimp_S', 
-            'Pudding_N','OctopusWiener_N', 
-            'Pudding_S', 'OctopusWiener_S', 
-            'RE_L', 'RE_R', 'RE_B',
-            'BL_L', 'BL_R', 'BL_B']
-
+                    'Tomato_N', 'Omelette_N',
+                    'Tomato_S', 'Omelette_S', 
+                    'FriedShrimp_N','FriedShrimp_W', 
+                    'FriedShrimp_E','FriedShrimp_S', 
+                    'Pudding_N','OctopusWiener_N', 
+                    'Pudding_S', 'OctopusWiener_S']
+        robo_maker_list = ['RE_L', 'RE_R', 'RE_B',
+                            'BL_L', 'BL_R', 'BL_B']
+        # blue ならま
+        if self.side == 'b':
+            self.markers_name_list.reverse()
+        
+        self.markers_name_list = self.markers_name_list+robo_maker_list
         # sub
         self.war_state=None
         self.war_state_sub = rospy.Subscriber('/war_state', String, self.warstate_callback)
@@ -36,8 +42,14 @@ class PubScore():
         self.war_state = data
 
         result = self.check_marker(self.war_state)
+        # blue なら値反転
+        if self.side == 'b':
+            result = map(lambda x: x *-1, result)
+
         _array.data = result
-        # print("pub score ", result)
+        print('*************************', self.markers_name_list)
+        print("pub score ", result)
+        print("self.side ", self.side)
         self.score_pub.publish(_array)
         
 
